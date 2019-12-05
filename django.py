bc7dbc0d025b5444d826138ab690b2c94878d43a
cng00006 <55862984+cng00006@users.noreply.github.com>
2019-12-04 22:49:22 -0800

    Update django.py

--- django.py
+++ django.py
@@ -33,3 +33,20 @@ def django_start():
     os.sytem('source /opt/django/django-env/bin/activate && echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\',\"admin@newproject.com\',\'NTI300NTI300\')" | python manage.py shell')
     
     outputwithnewline = subprocess.check_output('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Adress: //\' -e \'s/<.*$//\' ', shell=True)
+    print outputwithnewline
+    output = outputwithnewline.replace("\n", "")
+    old_string = "ALLOWED_HOSTS = []"
+    new_string = 'ALLOWED_HOSTS = [\'{}\']'.format(output)
+    print (new_string)
+    print (old_string)
+          
+    with open('project1/settings.py') as f:
+      newText=f.read().replace(old_string, new_string)
+    with open('project1/settings.py', "w") as f:
+      f.write(newText)
+          
+    os.system('sudo -u chrisng sh -c "source /opt/django/django-env/bin/activate && python manage.py runserver 0.0.0.0:8000&"')      
+          
+setup_install()
+django_install()
+django_start()          
